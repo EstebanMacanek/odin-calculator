a9f1bfebe4d1f1b5cff00a7233d89a3a70a0cc3d
Esteban Macanek <estebanmacanek@hotmail.com>
2022-11-09 00:34:19 -0300

    Make the screen work

--- script.js
+++ script.js
@@ -17,3 +17,13 @@ function divide (x, y) {
 function operate (operator, x, y) {
     return operator(x, y)
 }
+
+let screen = document.querySelector('.screen');
+let numbers = document.querySelectorAll('.numbers > button, .operators > button');
+let display = '';
+numbers.forEach(button => {
+    button.addEventListener('click', element => {
+        display += element.target.textContent;
+        screen.value = display;
+    });
+});
