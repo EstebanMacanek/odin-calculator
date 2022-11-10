eb8b1cc43dfbeeb6e147d39a85999390c8430a0e
Esteban Macanek <estebanmacanek@hotmail.com>
2022-11-10 09:42:57 -0300

    Add parenthesis

--- script.js
+++ script.js
@@ -19,13 +19,11 @@ function operate (operator, num1, num2) {
 }
 
 function separate (display) {
-    // '1 + 1' -> ['1', '+', '1']
-    let displayArray = display.split(' ');
+    let displayArray = display.trim().split(/\s+/);
     return displayArray
 }
 
 function translate (displayArray) {
-    // ['1', '+', '1'] -> [1, add, 1]
     let translatedDisplayArray = displayArray.map(item => {
         if (item === '+') {
             return add;
@@ -39,6 +37,12 @@ function translate (displayArray) {
         else if (item === '/') {
             return divide;
         }
+        else if (item === '(') {
+            return '('
+        }
+        else if (item === ')') {
+            return ')'
+        }
         else {
             return Number(item);
         }
@@ -46,19 +50,39 @@ function translate (displayArray) {
     return translatedDisplayArray
 }
 
-function indexOfAll (array, item) {
-    return array.reduce((a, e, i) => {
-        if (e === item) {
-            a.push(i)
-        } 
+function computeParenthesis (x) {
+    let lvl = 0;
+    return x.reduce((a, e) => {
+        if  (e === '(') {
+            lvl += 1;
+            if (lvl === 1) {
+                a.push([]);
+            }
+            else {
+                a[a.length - 1].push(e);
+            }
+        }
+        else if (e === ')') {
+            lvl -= 1;
+            if (lvl === 0) {
+                a[a.length - 1] = compute(order(a[a.length - 1]));
+            }
+            else {
+                a[a.length - 1].push(e);
+            }
+        }
+        else if (lvl >= 1) {
+            a[a.length - 1].push(e);
+        }
+        else {
+            a.push(e);
+        }
         return a
-    }, []);
+    }, [])
 }
 
 function order (x) {
-    // [1] -> [1]
-    // [1, add, 1] -> [[1], add, [1]]
-    // [1, add, 1, multiply, 2] -> [[1], add, [[1], multiply, [2]]]
+    x = computeParenthesis(x);
     if (x.length === 1) {
         return x
     }
@@ -66,10 +90,10 @@ function order (x) {
         return [x]
     }
     else {
-        for (let j of [add, subtract, multiply, divide]) {
-            let listIndex = indexOfAll(x, j);
-            for (let i of listIndex) {
-                x = [order(x.slice(0, i)), j, order(x.slice(i+1))]
+        for (let operator of [add, subtract, multiply, divide]) {
+            let index = x.lastIndexOf(operator);
+            if (index !== -1) {
+                x = [order(x.slice(0, index)), operator, order(x.slice(index + 1))]
             }
         }
         return x
@@ -77,8 +101,6 @@ function order (x) {
 }
 
 function compute (orderedTranslatedDisplayArray) {
-    // [1] -> 1
-    // [[1], add, [1]] -> 2
     if (orderedTranslatedDisplayArray.length === 1) {
         let num = orderedTranslatedDisplayArray[0];
         return num
@@ -92,67 +114,10 @@ function compute (orderedTranslatedDisplayArray) {
 }
 
 function calculate (display) {
-    return compute(order(translate(separate(display))))
+    let result = compute(order(translate(separate(display))));
+    let roundedResult = parseFloat(result.toFixed(2));
+    return roundedResult.toString()
 }
-
-// function separate (str) {
-//     let list = Array.from(str);
-//     let r = list.reduce((accumulator, currentValue) => {
-//         if (['.','0','1','2','3','4','5','6','7','8','9'].includes(currentValue)) {
-//             if (accumulator[accumulator.length-1] === '-' && accumulator[accumulator.length-2] === undefined) {
-//                 accumulator[accumulator.length - 1] += currentValue;
-//             }
-//             else if (accumulator.length === 0 || ['+', '-', '*', '/'].includes(accumulator[accumulator.length - 1])) {
-//                 accumulator.push(currentValue);
-//             }
-//             else {
-//                 accumulator[accumulator.length - 1] += currentValue;
-//             }
-//         }
-//         else if (['+', '-', '*', '/'].includes(currentValue)) {
-//             accumulator.push(currentValue);
-//         }
-//         return accumulator
-//     }, []);
-//     return r
-// }
-//
-// function compute (str) {
-//         let list = separate(str);
-//         for (let [index, item] of list.entries()) {
-//                 if (item === '*') {
-//                         list[index] = multiply(+list[index-1], +list[index+1]);
-//                         delete list[index-1];
-//             list.splice(index+1, 1)
-//         }
-//     }
-//     list = list.filter(a => a !== undefined);
-//     for (let [index, item] of list.entries()) {
-//             if (item === '/') {
-//                     list[index] = divide(+list[index-1], +list[index+1]);
-//                     delete list[index-1];
-//                     list.splice(index+1, 1)
-//                 }
-//             }
-//             list = list.filter(a => a !== undefined);
-//             for (let [index, item] of list.entries()) {
-//                     if (item === '+') {
-//                             list[index] = add(+list[index-1], +list[index+1])
-//                             delete list[index-1];
-//                             list.splice(index+1, 1)
-//                         }
-//                     }
-//                     list = list.filter(a => a !== undefined);
-//                     for (let [index, item] of list.entries()) {
-//                             if (item === '-'){
-//                                     list[index] = subtract(+list[index-1], +list[index+1])
-//                                     delete list[index-1];
-//                                     list.splice(index+1, 1)
-//                                 }
-//     }
-//     list = list.filter(a => a !== undefined);
-//     return String(parseFloat((+list[list.length-1]).toFixed(3)))
-// }
 
 let screen = document.querySelector('.screen');
 let display = '';
@@ -184,5 +149,11 @@ equals.addEventListener('click', () => {
 let clear = document.querySelector('.clear');
 clear.addEventListener('click', () => {
     display = '';
+    screen.textContent = display;
+});
+
+let del = document.querySelector('.del');
+del.addEventListener('click', () => {
+    display = display.slice(0, -1);
     screen.textContent = display;
 });
